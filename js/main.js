89447c2c4b2d70ebcc56369c30dccf4e78634d32
FrancescaBambozzi <bambozzif@gmail.com>
2018-09-14 16:25:36 +0200

    ciao

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/* Reveal SECTION-TITLES when scroll*/
+
+ScrollReveal().reveal('.section-title, h1', { delay: 300 });
+
+/* it makes CONTACT ICONS appear consecutively with 1 second interval */
+ScrollReveal().reveal('#contact1', { delay: 500 });
+ScrollReveal().reveal('#contact2', { delay: 700 });
+ScrollReveal().reveal('#contact3', { delay: 900 });
+
+/* it makes SKILL DIV appear consecutively with 1 second interval */
+ScrollReveal().reveal('#html-skill', { delay: 400 });
+ScrollReveal().reveal('#css-skill', { delay: 600 });
+ScrollReveal().reveal('#java-skill', { delay: 800 });
+ScrollReveal().reveal('#wordpress-skill', { delay: 1000 });
